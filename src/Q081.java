ad5ecb7a0c64d1c71d15f513a5799945623d69c4
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-05 16:38:12 +0800

    Add new files

--- src/Q081.java
+++ src/Q081.java
@@ -18,7 +18,7 @@ public class Q081 {
 //B，synchronized 關鍵字：當你在方法聲明前加上 synchronized，Java 會為這個方法加鎖。當某個線程執行該方法時，其他線程必須等待，直到這個線程執行完畢並釋放鎖。這樣就能防止多個線程同時修改共享變數 a、b 和 c，避免競爭條件的發生。
 
 /*
-Which makes class A thread safe?
+Which makes class A thread safe? 如何確保class A 執行序的安全?
 A. Class A is thread safe.
 B. Make foo and setB synchronized.
 C. Make foo synchronized.
