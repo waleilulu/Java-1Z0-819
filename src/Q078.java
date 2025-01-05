4096b0256c254292b997f320beb471be58b716d6
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-05 16:13:08 +0800

    Add new files

---
+++ src/Q078.java
@@ -0,0 +1,11 @@
+public class Q078 {
+    
+    //A
+    /*
+    Which module defines the foundational APIs of the Java SE Platform?
+    A. java.base
+    B. java.se
+    C. java.lang
+    D. java.object
+    */
+}
