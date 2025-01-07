492e3b728d0384fd83a0d1b56f9009316762f113
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-07 16:29:03 +0800

    Add new files

---
+++ src/Q094.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Q094 {
+public static void main(String[] args) {
+    var names = new ArrayList<>();
+    //var _ = 100;
+    var var = "hello";
+    //var y = null;
+    //var a;
+}
+
+//A、C
+/*
+Which two var declarations are correct? (Choose two.)
+A. var names = new ArrayList<>();
+B. var _ = 100;
+C. var var = “hello”;
+D. var y = null;
+E. var a;
+*/
+}
