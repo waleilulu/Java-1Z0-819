38522b78f591aacb3d6abc2a7ee65cde5db339e9
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-07 16:18:53 +0800

    Add new files

---
+++ src/Q093.java
@@ -0,0 +1,17 @@
+public class Q093 {
+    public static void main(String[] args) {
+        int i = 3;
+        int j = 25;
+        System.out.println(i>2?i>10?i*(j+10):i*j+5:1); //80   
+    }
+}
+
+//D
+/*
+What is the result?
+A. 385
+B. 3
+C. The compilation fails.
+D. 80
+E. 25
+*/
