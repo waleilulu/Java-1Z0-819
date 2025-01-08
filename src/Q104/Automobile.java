027f43bab296496214ef0e62162fe6459245bbc0
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-09 01:14:49 +0800

    Add new files

---
+++ src/Q104/Automobile.java
@@ -0,0 +1,5 @@
+package Q104;
+
+public abstract class Automobile { //line 1
+    abstract void wheels();
+}
---
+++ src/Q104/Car.java
@@ -0,0 +1,23 @@
+package Q104;
+
+public class Car extends Automobile{
+                        // line 2       
+    //void wheels(int i){ // line 3
+    //A
+    void wheels(){
+        System.out.print(4);
+    }
+    public static void main(String[] args) {
+        Automobile ob = new Car(); // line 4
+        ob.wheels();
+    }
+
+}
+
+/*
+What must you do so that the code prints 4?
+A. Remove the parameter from wheels method in line 3.
+B. Remove abstract keyword in line 1.
+C. Replace the code in line 2 with Car ob = new Car();.
+D. Add @Override annotation at line 2.
+*/
