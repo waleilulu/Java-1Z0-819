29d5263ee55d7bc2e00e9e2953fd0bd6e3546b6d
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-09 01:21:13 +0800

    Add new files

---
+++ src/Q106.java
@@ -0,0 +1,11 @@
+public class Q106 {
+
+//B
+/*
+Which set of commands is necessary to create and run a custom runtime image from Java source files?
+A. java,jdeps
+B. javac,jlink
+C. javac,jar
+D. jar,jlink
+*/
+}
