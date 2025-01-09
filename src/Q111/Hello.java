17c9f5ca6243334ad7bbdf38be680bca9e9c1e6b
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-10 01:45:15 +0800

    Add new files

---
+++ src/Q111/Hello.java
@@ -0,0 +1,53 @@
+package Q111;
+
+public class Hello {
+    public static void main(String[] args) {
+        Module module = Hello.class.getModule();
+        System.out.println("Module: " + module);
+        System.out.println("Name: " + module.getName());
+        System.out.println("Description: " + module.getDescriptor());
+        
+    }
+}
+
+
+//javac -d Q111 src\Q111\Hello.java
+//java -cp Q111 Q111.Hello
+/* 得出
+Module: unnamed module @5ecddf8f
+Name: null
+Description: null
+*/
+
+
+//D
+/*
+Given the directory structure:
+\Test
+    Hello.java
+
+Given the commands to execute at the Test directory prompt:
+Test>javac -d Q111 Hello.java
+Test>javac -cp Q111 Q111.Hello
+
+Which statement is true?
+A. Create an empty module-info.java file in the Test directory and on execution of the given commands, the program prints:
+    Module: unnamed module @<</font><</font>hash code>>
+    Name: null
+    Descriptor: module-info
+
+B. On execution of the given commands, the program prints:
+    Module: Q111.Hello @<</font><</font>hash code>>
+    Name: unnamed
+    Descriptor: null
+
+C. Execute java --module-path pac pac.Hello instead of java -cp pac pac.Hello and on execution the program prints:
+    Module: Q111 @<</font><</font>hash code>>
+    Name: Q111.Test
+    Descriptor: null
+
+D. On execution of the given commands, the program prints:
+    Module: unnamed module @<</font><</font>hash code>>
+    Name: null
+    Descriptor: null
+*/
