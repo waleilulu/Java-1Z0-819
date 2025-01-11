4caf2d86a7ec4c83e0d1bdfb36db6b2a2907c263
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-11 22:50:13 +0800

    Add new files

--- src/Q124.java
+++ src/Q124.java
@@ -4,9 +4,9 @@ import java.util.Comparator;
 public class Q124 {
     public static void main(String[] args) {
         String[] towns = {"boston", "paris", "bangkok", "oman"};
-        Comparator<String> ms = (a, b) -> b.compareTo(a);
-        Arrays.sort(towns, ms);
-        System.out.println(Arrays.binarySearch(towns, "oman", ms));
+        Comparator<String> ms = (a, b) -> b.compareTo(a); //b.compareTo(a) 則用於實現降序排列的比較邏輯。
+        Arrays.sort(towns, ms); //["paris", "oman", "boston", "bangkok"]
+        System.out.println(Arrays.binarySearch(towns, "oman", ms)); //1
     }
 }
 
