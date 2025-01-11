ee2f1caf3482486a4adc72748243a6f3454f939d
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-11 22:58:25 +0800

    Add new files

---
+++ src/Q125.java
@@ -0,0 +1,11 @@
+public class Q125 {
+/*
+Which statement is true?
+A. PrintWriter outputs characters and automatically flushes the stream.
+B. System.exit() invokes the close() method for the InputStream/OutputStream resources.
+C. Console.readPassword() method encrypts the text entered.
+D. PrintStream outputs only bytes.
+*/
+
+}
+//A， A 不完全準確，但相較於其他選項來說，它是最接近正確的答案。在某些情況下，PrintWriter 確實可以設定為自動刷新，這點符合敘述中的「自動刷新」條件，因此選擇 A 作為正確答案。
