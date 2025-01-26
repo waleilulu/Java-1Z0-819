6f59ac7c5ef38f15804bf7ed913be5ddd7252a13
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2025-01-26 16:12:37 +0800

    Add new files

--- src/Q204.java
+++ src/Q204.java
@@ -5,7 +5,7 @@ public class Q204 {
         // A. 這一行會編譯錯誤，因為無法將 `final` 和 `var` 混合使用
         // BiPredicate<Integer, Integer> testA = (Integer x, final var y) -> (x.equals(y)); // 編譯錯誤
 
-        // B. 這一行會編譯成功，因為 `x` 和 `y` 顯式聲明為 `Integer`，並且 `y` 被標註為 `final`
+        // B. 這一行會編譯成功，因為 `x` 和 `y` 顯示聲明為 `Integer`，並且 `y` 被標註為 `final`
         BiPredicate<Integer, Integer> testB = (Integer x, final Integer y) -> (x.equals(y));
         System.out.println(testB.test(1, 1)); // 應該會輸出 true，因為兩者相等
 
